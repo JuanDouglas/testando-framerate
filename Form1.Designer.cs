cfae49bf1ed3cae583f2511ba37da33da216ac0d
Juan <juandouglas2004@gmail.com>
2020-06-30 20:01:15 -0300

    Start

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Testando_Framerates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelTeste = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbFramerate = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbColorido = new System.Windows.Forms.CheckBox();
+            this.btnAleatorio = new System.Windows.Forms.Button();
+            this.PanelAleatorio = new System.Windows.Forms.Panel();
+            this.btnStartAleatorio = new System.Windows.Forms.Button();
+            this.tbMaximo = new System.Windows.Forms.TextBox();
+            this.tbIncial = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PanelTeste.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.PanelAleatorio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelTeste
+            // 
+            this.PanelTeste.BackColor = System.Drawing.Color.Black;
+            this.PanelTeste.Controls.Add(this.pictureBox1);
+            this.PanelTeste.Location = new System.Drawing.Point(12, 12);
+            this.PanelTeste.Name = "PanelTeste";
+            this.PanelTeste.Size = new System.Drawing.Size(236, 133);
+            this.PanelTeste.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Testando_Framerates.Properties.Resources.galinha__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(236, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbFramerate
+            // 
+            this.tbFramerate.Location = new System.Drawing.Point(52, 154);
+            this.tbFramerate.Name = "tbFramerate";
+            this.tbFramerate.Size = new System.Drawing.Size(100, 20);
+            this.tbFramerate.TabIndex = 1;
+            this.tbFramerate.TextChanged += new System.EventHandler(this.tbFramerate_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(173, 152);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "iniciar";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hz:";
+            // 
+            // cbColorido
+            // 
+            this.cbColorido.AutoSize = true;
+            this.cbColorido.Location = new System.Drawing.Point(15, 209);
+            this.cbColorido.Name = "cbColorido";
+            this.cbColorido.Size = new System.Drawing.Size(64, 17);
+            this.cbColorido.TabIndex = 4;
+            this.cbColorido.Text = "Colorido";
+            this.cbColorido.UseVisualStyleBackColor = true;
+            // 
+            // btnAleatorio
+            // 
+            this.btnAleatorio.Location = new System.Drawing.Point(178, 180);
+            this.btnAleatorio.Name = "btnAleatorio";
+            this.btnAleatorio.Size = new System.Drawing.Size(70, 23);
+            this.btnAleatorio.TabIndex = 5;
+            this.btnAleatorio.Text = "Aleatorio";
+            this.btnAleatorio.UseVisualStyleBackColor = true;
+            this.btnAleatorio.Click += new System.EventHandler(this.btnAleatorio_Click);
+            // 
+            // PanelAleatorio
+            // 
+            this.PanelAleatorio.Controls.Add(this.btnStartAleatorio);
+            this.PanelAleatorio.Controls.Add(this.tbMaximo);
+            this.PanelAleatorio.Controls.Add(this.tbIncial);
+            this.PanelAleatorio.Controls.Add(this.label3);
+            this.PanelAleatorio.Controls.Add(this.label2);
+            this.PanelAleatorio.Location = new System.Drawing.Point(15, 232);
+            this.PanelAleatorio.Name = "PanelAleatorio";
+            this.PanelAleatorio.Size = new System.Drawing.Size(238, 82);
+            this.PanelAleatorio.TabIndex = 6;
+            this.PanelAleatorio.Visible = false;
+            // 
+            // btnStartAleatorio
+            // 
+            this.btnStartAleatorio.Enabled = false;
+            this.btnStartAleatorio.Location = new System.Drawing.Point(158, 40);
+            this.btnStartAleatorio.Name = "btnStartAleatorio";
+            this.btnStartAleatorio.Size = new System.Drawing.Size(75, 23);
+            this.btnStartAleatorio.TabIndex = 4;
+            this.btnStartAleatorio.Text = "iniciar";
+            this.btnStartAleatorio.UseVisualStyleBackColor = true;
+            this.btnStartAleatorio.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbMaximo
+            // 
+            this.tbMaximo.Location = new System.Drawing.Point(52, 42);
+            this.tbMaximo.Name = "tbMaximo";
+            this.tbMaximo.Size = new System.Drawing.Size(100, 20);
+            this.tbMaximo.TabIndex = 3;
+            this.tbMaximo.TextChanged += new System.EventHandler(this.tbFramerate_TextChanged);
+            // 
+            // tbIncial
+            // 
+            this.tbIncial.Location = new System.Drawing.Point(52, 11);
+            this.tbIncial.Name = "tbIncial";
+            this.tbIncial.Size = new System.Drawing.Size(100, 20);
+            this.tbIncial.TabIndex = 2;
+            this.tbIncial.TextChanged += new System.EventHandler(this.tbFramerate_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Maximo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Inicial";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(106, 180);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(61, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Parar";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "imagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 322);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.PanelAleatorio);
+            this.Controls.Add(this.btnAleatorio);
+            this.Controls.Add(this.cbColorido);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbFramerate);
+            this.Controls.Add(this.PanelTeste);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Testando Framerate";
+            this.PanelTeste.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.PanelAleatorio.ResumeLayout(false);
+            this.PanelAleatorio.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelTeste;
+        private System.Windows.Forms.TextBox tbFramerate;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbColorido;
+        private System.Windows.Forms.Button btnAleatorio;
+        private System.Windows.Forms.Panel PanelAleatorio;
+        private System.Windows.Forms.Button btnStartAleatorio;
+        private System.Windows.Forms.TextBox tbMaximo;
+        private System.Windows.Forms.TextBox tbIncial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Timer = System.Timers.Timer;
+
+namespace Testando_Framerates
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        Image[] gif;
+        int gifAtual;
+        int gifRe;
+        bool Imagem;
+        public Form1()
+        {
+            InitializeComponent();
+            btnStop.Enabled = false;
+            btnStart.Enabled = false;
+            pictureBox1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = true;
+            int inframerate =int.Parse(tbFramerate.Text);
+            double framerate = Convert.ToDouble(new TimeSpan(0,0,0,1).TotalMilliseconds /inframerate);
+
+            if (timer!=null)
+            {
+                timer.Enabled = false;
+                timer = null;
+            }
+            timer = new Timer(framerate);
+            timer.Enabled = true;
+
+                timer.Elapsed += new ElapsedEventHandler((object sender2, ElapsedEventArgs @event) => {
+                    Piscar();
+                });
+
+        }
+        private void Piscar() {
+            if (!Imagem)
+            {
+                if (!cbColorido.Checked)
+                {
+                    if (PanelTeste.BackColor == Color.Black)
+                    {
+                        PanelTeste.BackColor = Color.White;
+                    }
+                    else
+                    {
+                        PanelTeste.BackColor = Color.Black;
+                    }
+                }
+                else
+                {
+                    Random rd = new Random();
+                    PanelTeste.BackColor = Color.FromArgb(rd.Next(0, 255), rd.Next(0, 255), rd.Next(0, 255), rd.Next(0, 255));
+                }
+            }
+            else
+            {
+                Update();
+            }
+        }
+        private void PiscarAleatorio()
+        {
+            Random rd = new Random();
+
+            int inframerate = rd.Next(int.Parse(tbIncial.Text), int.Parse(tbMaximo.Text));
+            double framerate = Convert.ToDouble(new TimeSpan(0, 0, 0, 1).TotalMilliseconds / inframerate);
+            Piscar();
+            timer.Interval = framerate;
+        }
+
+        private void tbFramerate_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var tb = (TextBox)sender;
+                int con = int.Parse(tb.Text);
+                if (PanelAleatorio.Visible==false)
+                {
+                    if (con >= 1)
+                    {
+                        btnStart.Enabled = true;
+                    }
+                }
+                else
+                {
+                    if (con >= 1)
+                    {
+                        btnStartAleatorio.Enabled = true;
+                    }
+                    try
+                    {
+                        int max = int.Parse(tbMaximo.Text);
+                        int min = int.Parse(tbIncial.Text);
+                        if (min < 1)
+                        {
+                            btnStartAleatorio.Enabled = false;
+                        }
+                        else if (max<min)
+                        {
+                            btnStartAleatorio.Enabled = false;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        btnStartAleatorio.Enabled = false;
+                    }
+                    
+                }
+
+            }
+            catch (Exception)
+            {
+                if (PanelAleatorio.Visible == false)
+                {
+                    btnStart.Enabled = false;
+                }
+                else
+                {
+                    btnStartAleatorio.Enabled = false;
+                }
+            }
+        }
+
+        private void btnAleatorio_Click(object sender, EventArgs e)
+        {
+            if (PanelAleatorio.Visible==false)
+            {
+                PanelAleatorio.Visible = true;
+                btnStart.Enabled = false;
+                tbFramerate.Enabled = false;
+            }
+            else
+            {
+                tbFramerate.Enabled = true;
+                btnStart.Enabled = true;
+                PanelAleatorio.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = true;
+            if (timer != null)
+            {
+                timer.Enabled = false;
+                timer = null;
+            }
+            timer = new Timer(1);
+            timer.Enabled = true;
+                timer.Elapsed += new ElapsedEventHandler((object sender2, ElapsedEventArgs @event) => {
+                    PiscarAleatorio();
+                });
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            btnStop.Enabled = false;
+            button1.Enabled = true;
+        }
+        private Image[] Seleciona(string selecionado) {
+            Image[] gif;
+            switch (selecionado)
+            {
+                case "bob":
+                    gif = new Image[6];
+                    gif[0] = Properties.Resources.bob__1_;
+                    gif[1] = Properties.Resources.bob__2_;
+                    gif[2] = Properties.Resources.bob__3_;
+                    gif[3] = Properties.Resources.bob__4_;
+                    gif[4] = Properties.Resources.bob__5_;
+                    gif[5] = Properties.Resources.bob__6_;
+                    break;
+                case "coracao":
+                    gif = new Image[13];
+                    gif[0] = Properties.Resources.coracao__1_;
+                    gif[1] = Properties.Resources.coracao__2_;
+                    gif[2] = Properties.Resources.coracao__3_;
+                    gif[3] = Properties.Resources.coracao__4_;
+                    gif[4] = Properties.Resources.coracao__5_;
+                    gif[5] = Properties.Resources.coracao__6_;
+                    gif[6] = Properties.Resources.coracao__7_;
+                    gif[7] = Properties.Resources.coracao__8_;
+                    gif[8] = Properties.Resources.coracao__9_;
+                    gif[9] = Properties.Resources.coracao__10_;
+                    gif[10] = Properties.Resources.coracao__11_;
+                    gif[11] = Properties.Resources.coracao__12_;
+                    gif[12] = Properties.Resources.coracao__13_;
+                    break;
+                default:
+                    gif = new Image[12];
+                    gif[0] = Properties.Resources.galinha__1_;
+                    gif[1] = Properties.Resources.galinha__2_;
+                    gif[2] = Properties.Resources.galinha__3_;
+                    gif[3] = Properties.Resources.galinha__4_;
+                    gif[4] = Properties.Resources.galinha__5_;
+                    gif[5] = Properties.Resources.galinha__6_;
+                    gif[6] = Properties.Resources.galinha__7_;
+                    gif[7] = Properties.Resources.galinha__8_;
+                    gif[8] = Properties.Resources.galinha__9_;
+                    gif[9] = Properties.Resources.galinha__10_;
+                    gif[10] = Properties.Resources.galinha__11_;
+                    gif[11] = Properties.Resources.galinha__12_;
+                    break;
+            }
+            return gif;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string[] values = new string[] { "bob", "coracao", "g" };
+            if (gifRe == null)
+            {
+                gifAtual = 0;
+            }
+            if (gifRe == values.Length - 1)
+            {
+                gifRe = 0;
+            }
+            else
+            {
+                gifRe++;
+            }
+            gif = Seleciona(values[gifRe]);
+            
+            if (Imagem)
+            {
+                pictureBox1.Visible =false;
+                Imagem = false;
+                button1.Enabled = true;
+                PanelTeste.BackColor = this.BackColor;
+            }
+            else
+            {
+                PanelTeste.BackColor = this.BackColor;
+                pictureBox1.Visible = true;
+                Imagem = true;
+                button1.Enabled = false;
+            }
+        }
+        private void Update() {
+            if (gifAtual==null)
+            {
+                gifAtual = 0;
+            }
+            try
+            {
+                pictureBox1.Image = gif[gifAtual];
+            }
+            catch (Exception)
+            {
+                pictureBox1.Image = gif[0];
+                gifAtual = 0;
+            }
+            if (gifAtual==gif.Length-1)
+            {
+                gifAtual =0;
+            }
+            else
+            {
+                gifAtual++;
+            }
+        }
+    }
+}
